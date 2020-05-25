0fc1f997086614c4b3e665612704a27127ad1a79
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-05-25 19:56:16 +0500

    two nums start

--- add_two_numbers/add_two_numbers.py
+++ add_two_numbers/add_two_numbers.py
@@ -11,4 +11,11 @@
  * @return {ListNode}
 """
 def addTwoNumbers(l1, l2):
-    pass
+    # Go down both lists at the same time, sum the elements, store the result in a new node
+    # if sum is bigger than 9 (cause ech entry must be a digit) take the extra 1 and add it to the next sum
+    while l1.next is not None or l2.next is not None:
+        if l1 and l2:
+            # casual sum
+        elif l1.next:
+            # l1 + carry from prev pair
+        elif l2.next:
