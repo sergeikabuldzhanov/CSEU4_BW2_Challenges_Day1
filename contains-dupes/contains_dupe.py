3c04b47730860692b5b9789b9136f0799549f297
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-05-26 13:27:45 +0500

    array has duplicates

--- contains-dupes/contains_dupe.py
+++ contains-dupes/contains_dupe.py
@@ -1,2 +1,12 @@
 def containsDuplicate(num):
-    pass
+    lookup = set()
+    # go through the array, and add numbers to a lookup table
+    # if any number we encounter has already been added to the lookup
+    # it means it's a duplicate, and the array has duplicates
+    # complexity of O(n)
+    for number in num:
+        if number not in lookup:
+            lookup.add(number)
+        else:
+            return True
+    return False
